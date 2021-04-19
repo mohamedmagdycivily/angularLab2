e81b4222c6d09519d1c31cabfbc1830e689876c8
mohamedmagdycivily <mohamedmagdycivily@gmail.com>
2021-04-19 16:18:10 +0200

    add students & reg components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FirstComponent } from './Components/First/first.component';
 import { SecondComponent } from './Components/second/second.component';
+import { RegComponent } from './Components/lab3/reg/reg.component';
+import { StudentComponent } from './Components/lab3/student/student.component';
+import { StudentsComponent } from './Components/lab3/students/students.component';
 
 @NgModule({
-  declarations: [AppComponent, FirstComponent, SecondComponent],
+  declarations: [AppComponent, FirstComponent, SecondComponent, RegComponent, StudentComponent, StudentsComponent],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
