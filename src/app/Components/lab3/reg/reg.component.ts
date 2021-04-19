755fff608f61508db6773091c6b8d84567f5f3b4
mohamedmagdycivily <mohamedmagdycivily@gmail.com>
2021-04-19 17:35:12 +0200

    send data from child to parent

--- src/app/Components/lab3/reg/reg.component.ts
+++ src/app/Components/lab3/reg/reg.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-reg',
   templateUrl: './reg.component.html',
-  styleUrls: ['./reg.component.css']
+  styleUrls: ['./reg.component.css'],
 })
 export class RegComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
+  name: string = '';
+  age: number = null;
+  student: { sname: string; sage: number } = {
+    sname: this.name,
+    sage: this.age,
+  };
 
-  ngOnInit(): void {
+  @Output() myEvent = new EventEmitter();
+
+  send() {
+    // this.student.sname = this.name;  //error undefind
+    // this.student.sage = this.age;    //error undefind
+    this.myEvent.emit(this.student);
+
+    // console.log(this.name);
+    // console.log(this.age);
   }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,16 @@ import { AppComponent } from './app.component';
 import { FirstComponent } from './Components/First/first.component';
 import { SecondComponent } from './Components/second/second.component';
 import { RegComponent } from './Components/lab3/reg/reg.component';
-import { StudentComponent } from './Components/lab3/student/student.component';
 import { StudentsComponent } from './Components/lab3/students/students.component';
 
 @NgModule({
-  declarations: [AppComponent, FirstComponent, SecondComponent, RegComponent, StudentComponent, StudentsComponent],
+  declarations: [
+    AppComponent,
+    FirstComponent,
+    SecondComponent,
+    RegComponent,
+    StudentsComponent,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
